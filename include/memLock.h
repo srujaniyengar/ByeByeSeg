5942ed9d1ca59826b1c32ba060dae358c3cc50c9
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-05-27 14:08:33 +0530

    chore(memLock): branch it out

--- include/memLock.h
+++ include/memLock.h
@@ -1,34 +1,9 @@
 #ifndef MEMLOCK_H
 #define MEMLOCK_H
-#include <stdio.h>
 #include <stdlib.h>
-/* THIS LIBRARY IS TO ADD BORROW CHECKER FEATURE FOR C 
- * THESE ARE DESIGNED TO AVOID PHANTOM READ/WRITE
- * Ensures memory safety without needing a garbage collector. It enforces strict rules 
- * about how references to data are used, preventing issues like dangling pointers, 
- * data races, and double frees at compile time.
- */
-
-// Borrow Checker Enum
-enum Mutability {
-    mut,   // Mutable reference
-    immut  // Immutable reference
-};
-
-// TO BE ADDED AT THE BEGINNING OF ANY FUNCTION
-void start_check(); 
-
-// TO BE ADDED AT THE END OF ANY FUNCTION
-void end_check();  
-
-// TO BORROW MEMORY IN A FUNCTION
-void borrow(void* ptr, enum Mutability mut);  
-
-// TO RELEASE BORROWED MEMORY
-void release(void* ptr);  
-
-// CHECKS IF THERE IS A BORROW CONFLICT
-int is_conflict(void* ptr, enum Mutability mut);  
-
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h> 
 #endif // MEMLOCK_H
-
